2cb16ab417b151766123c4f37c60e998ce423413
Benja <benja.mora.torres@gmail.com>
2017-11-02 00:53:59 -0300

    Estructura

---
+++ Prueba2/capaNegocio/Empleado.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace capaNegocio
+{
+    public class Empleado
+    {
+        private int rut;
+
+public int Rut
+{
+  get { return rut; }
+  set { rut = value; }
+}
+        private char dv;
+
+public char Dv
+{
+  get { return dv; }
+  set { dv = value; }
+}
+        private String nombre;
+
+public String Nombre
+{
+  get { return nombre; }
+  set { nombre = value; }
+}
+        private String apellido;
+
+public String Apellido
+{
+  get { return apellido; }
+  set { apellido = value; }
+}
+        private int empresa;
+
+public int Empresa
+{
+  get { return empresa; }
+  set { empresa = value; }
+}
+
+
+        public Empleado()
+        {
+            Init();
+       }
+        private void Init(){
+            Rut = Convert.ToInt32(rut);
+            Dv  = Convert.ToChar(dv);
+            Nombre = nombre;
+            Apellido = apellido;
+            Empresa = Convert.ToInt32(empresa);
+        }
+
+
+}
---
+++ Prueba2/capaNegocio/Empresa.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace capaNegocio
+{
+    public class Empresa
+    {
+        private int rut;
+
+        public int Rut
+        {
+            get { return rut; }
+            set { rut = value; }
+        }
+
+        private char dv;
+
+        public char Dv
+        {
+            get { return dv; }
+            set { dv = value; }
+        }
+
+        private String razonSocial;
+
+        public String RazonSocial
+        {
+            get { return razonSocial; }
+            set { razonSocial = value; }
+        }
+
+       
+        public Empresa()
+        {
+            Init();
+       }
+        private void Init(){
+            Rut = Convert.ToInt32(rut);
+            Dv  = Convert.ToChar(dv);
+            RazonSocial = razonSocial;
+        }
+    }
+}
