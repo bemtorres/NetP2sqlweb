61875bd6c4cbbe139bdc22402403089a16ad33c7
Benja <benja.mora.torres@gmail.com>
2017-12-10 02:44:14 -0300

    123

--- Empresaxd/CapaNegocio/EmpleadoColeccion.cs
+++ Empresaxd/CapaNegocio/EmpleadoColeccion.cs
@@ -30,5 +30,33 @@ namespace CapaNegocio
             }
             return listaEmpleado;
         }
+
+        public static List<Empleado> generalListado(int empresa)
+        {
+            List<Empleado> listaEmpleado = new List<Empleado>();
+
+            EmpresasEntities modelo = new EmpresasEntities();
+
+            var empleados = modelo.Empleado;
+
+            foreach (CapaDatos.Empleado item in empleados.ToList())
+            {
+                if (item.Empresa== empresa)
+                {
+                     Empleado emp = new Empleado();
+
+                    emp.Rut = item.Rut;
+                    emp.Dv = Convert.ToChar(item.Dv);
+                    emp.Nombres = item.Nombres;
+                    emp.Apellidos = item.Apellidos;
+                    emp.Empresa = item.Empresa;
+                    listaEmpleado.Add(emp);
+                }
+               
+
+               
+            }
+            return listaEmpleado;
+        }
     }
 }
